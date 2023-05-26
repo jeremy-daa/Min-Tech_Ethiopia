f2dcbc4bda3b3c1fc6db7cc67791c2cc955528b6
Nahom Girma Woldemichael <nahomg761@gmail.com>
2023-05-26 09:32:42 +0300

    carousels have been updated

--- js/main.js
+++ js/main.js
@@ -358,34 +358,41 @@
 })(jQuery);
 
 // carousel
-var carousel = document.querySelector('.carousel');
-var images = carousel.querySelectorAll('img');
-var currentIndex = 0;
-var autoplayTimer;
-
-function showImage(index) {
-  images.forEach(function(image, i) {
-    if (i === index) {
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
+window.addEventListener('DOMContentLoaded', function() {
+    var carousels = document.querySelectorAll('.carousel');
+  
+    function showImage(carousel, index) {
+      var images = carousel.querySelectorAll('img');
+      images.forEach(function(image, i) {
+        if (i === index) {
+          image.style.display = 'block';
+        } else {
+          image.style.display = 'none';
+        }
+      });
     }
+  
+    function nextImage(carousel) {
+      var images = carousel.querySelectorAll('img');
+      var currentIndex = parseInt(carousel.dataset.currentIndex);
+      var newIndex = (currentIndex + 1) % images.length;
+      carousel.dataset.currentIndex = newIndex;
+      showImage(carousel, newIndex);
+    }
+  
+    function autoplayCarousel(carousel) {
+      var autoplayTimer = setInterval(function() {
+        nextImage(carousel);
+      }, 5000);
+      carousel.dataset.autoplayTimer = autoplayTimer;
+    }
+  
+    // Initialize carousels
+    carousels.forEach(function(carousel) {
+      carousel.dataset.currentIndex = 0;
+      showImage(carousel, 0);
+      autoplayCarousel(carousel);
+    });
   });
-}
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-}
-
-function autoplayCarousel() {
-  nextImage();
-  autoplayTimer = setTimeout(autoplayCarousel, 5000);
-}
-
-// Show the initial image
-showImage(currentIndex);
-
-// Start autoplay
-autoplayCarousel();
+  
 // carousel
